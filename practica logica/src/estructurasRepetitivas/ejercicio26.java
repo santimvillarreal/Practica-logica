81c34f6a6bcf3d1a9480c6dab1aaa818bb5e1fd7
Santiago Villarreal <santivilla795@hotmail.com>
2024-08-19 19:03:30 -0300

    ejercicio26

--- practica logica/src/estructurasRepetitivas/ejercicio26.java
+++ practica logica/src/estructurasRepetitivas/ejercicio26.java
@@ -1,5 +1,37 @@
 package estructurasRepetitivas;
 
+import java.util.Scanner;
+
 public class ejercicio26 {
-    
+    /*
+     *  Imprimir la serie de los “N” términos de la serie de Fibonacci. 
+     */
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Escriba cuantos numeros de la serie fibonacci desea imprimir >");
+        int numero = Integer.parseInt(entrada.nextLine());
+        int j = 1;
+        int i = 0;
+        int k=0;
+        int fibo = 0;
+        while(k<numero){
+            if(k==0){
+                System.out.println(i);
+                k++;
+            }else if(k==1){
+                System.out.println(j);
+                k++;
+            }
+            fibo = j + i;
+            System.out.println(fibo);
+            k++;
+            if(k>0 && k % 2 == 0){
+                j = fibo;
+            } else {
+                i= fibo;
+            }
+        }
+        entrada.close();
+    }
 }
